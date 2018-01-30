8d95589debb40ff0adc2963cb73a1645dc6dbe33
Antoine Rosse <rosse.antoine@gmail.com>
2018-01-30 15:53:30 +0100

    qr code reader without var errorElement and video

--- qrcode-reader/js/script.js
+++ qrcode-reader/js/script.js
@@ -47,5 +47,35 @@ function errorMsg(msg, error) {
   }
 }
 
-navigator.mediaDevices.getUserMedia(constraints).
+MediaStreamTrack.getSources(function(sourceInfos) {
+  var audioSource = null;
+  var videoSource = null;
+
+  for (var i = 0; i != sourceInfos.length; ++i) {
+    var sourceInfo = sourceInfos[i];
+    if (sourceInfo.kind === 'video') {
+      console.log(sourceInfo.id, sourceInfo.label || 'camera');
+
+      videoSource = sourceInfo.id;
+    } else {
+      console.log('Some other kind of source: ', sourceInfo);
+    }
+  }
+
+  sourceSelected(audioSource, videoSource);
+});
+
+function sourceSelected(audioSource, videoSource) {
+  var constraints = {
+    audio: {
+      optional: [{sourceId: audioSource}]
+    },
+    video: {
+      optional: [{sourceId: videoSource}]
+    }
+  };
+
+  navigator.mediaDevices.getUserMedia(constraints).
     then(handleSuccess).catch(handleError);
+}
+
