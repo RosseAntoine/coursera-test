530a757bc25e66b6080183ae86cca40caad9d8e8
Antoine Rosse <rosse.antoine@gmail.com>
2018-01-30 14:01:11 +0100

    qr code reader without var errorElement and video

--- qrcode-reader/js/script.js
+++ qrcode-reader/js/script.js
@@ -8,8 +8,8 @@
 
 // 'use strict';
 
-var errorElement = document.querySelector('#errorMsg');
-var video = document.querySelector('video');
+// var errorElement = document.querySelector('#errorMsg');
+// var video = document.querySelector('video');
 
 // Put variables in global scope to make them available to the browser console.
 var constraints = window.constraints = {
